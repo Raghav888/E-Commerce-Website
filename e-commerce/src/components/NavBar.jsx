617e1699cd9486b15ac7a1302f0edf57baf003fb
Raghav888 <raghavbang2012@gmail.com>
2023-03-29 20:37:44 +0530

    add navbar

---
+++ e-commerce/src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import styles from "../styles/NavBar.module.css";
+
+export const NavBar = () => {
+  return (
+    <nav className={styles['mantra-nav']}>
+      <div className={styles['mantra-nav-title']}>
+        <a className={styles['mantra-title']} href="../index.html">
+          E-Commerce
+        </a>
+      </div>
+      <div className={styles['mantra-nav-footer']}>
+        <div className={styles['mantra-badge']}>
+          <a className={styles['mantra-menu-icon']} href="#">
+            <span className={styles['mantra-icon']}>
+              <i className="fa fa-heart"></i>
+            </span>
+          </a>
+          <a className={styles['mantra-menu-icon']} href="#">
+            <span className={styles['mantra-icon']}>
+              <i className="fa fa-shopping-cart"></i>
+            </span>
+            <span className={styles['mantra-cart-button']}>Cart</span>
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
---
+++ e-commerce/src/pages/index.js
@@ -0,0 +1,10 @@
+import { NavBar } from '@/components/NavBar'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+ <NavBar/>
+  )
+}
