041f5885c982228f3e1035f7876cad09c370e688
Jagadeesh Gowthu <98765192+iamjagadeesh007@users.noreply.github.com>
2023-03-30 23:05:10 +0530

    fix: code refactor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,39 +1,65 @@
 import { useEffect, useState } from "react";
+import { usePosition } from "../common/usePosition";
 import styles from "../styles/Footer.module.css";
 
 export const Footer = () => {
-  const [temperature, setTemperature] = useState();
+  const {latitude, longitude} = usePosition();
+  const [temperature, setTemperature] = useState(0);
+  const [address, setAddress] = useState('');
 
-  useEffect(() => {
-    (async () => {
+  const fetchWeather = async () => {
+    try {
       let response = await fetch(
-        "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/delhi/today?unitGroup=metric&include=days&key=D32KVVAXE6QRG5K5BX25MBHX2&contentType=json"
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${latitude},${longitude}/today?unitGroup=metric&include=days&key=D32KVVAXE6QRG5K5BX25MBHX2&contentType=json`
       );
       response = await response.json();
-      let temp = response?.days[0]?.temp;
+      const temp = response?.days?.[0]?.temp || 0;
       setTemperature(temp);
-    })();
-  }, []);
+    } catch (error) {
+      setTemperature(0);
+    }
+  };
+
+  const fetchAddress = async () => {
+    try {
+      let response = await fetch(
+        `https://geocode.maps.co/reverse?lat=${latitude}&lon=${longitude}`
+      );
+      response = await response.json();
+      const currentAddress = response?.display_name || "";
+      setAddress(currentAddress);
+    } catch (error) {
+      setAddress("");
+    }
+  };
+
+  useEffect(() => {
+    if(latitude && longitude) {
+      fetchWeather();
+      fetchAddress();
+    }
+  }, [latitude, longitude]);
 
   return (
     <div className={styles.footer}>
-      <div className={styles.addressTab}>
-        <div className={styles.head}>
-          <h1>Address:</h1>
-        </div>
-        <div className={styles.address}>
-          <h2>Near XYZ, ABC Road,</h2>
-          <h2>Infront of Y Building, Delhi, India.</h2>
-        </div>
-      </div>
-      <div className={styles.temperature}>
-        <div className={styles.head}>
-          <h1>Live Temperature of Delhi:</h1>
-        </div>
-        <div className={styles.address}>
-          <h2>{temperature}&deg;C</h2>
-        </div>
-      </div>
+      {!(latitude && longitude) ? (
+        <>
+          <div className={styles.addressError}>
+            Geo Location not found
+          </div>
+        </>
+      ) : (
+        <>
+          <div className={styles.address}>
+            <div className={styles.addressHeader}>Current Address:</div>
+            <div>{address}</div>
+          </div>
+          <div className={styles.temperature}>
+            <span className={styles.temperatureHeader}>Temperature: </span>
+            {temperature}&deg;C
+          </div>
+        </>
+      )}
     </div>
   );
 };
