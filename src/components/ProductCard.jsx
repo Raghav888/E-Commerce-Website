5ddde6c1959594be188a32c9e4ba9ba035dcb12d
Raghav888 <raghavbang2012@gmail.com>
2023-03-30 07:57:58 +0530

    buy logic

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -9,25 +9,35 @@ export const ProductCard = () => {
     for (let i = 0; i < 20; i++) {
       const imageUrl = "https://picsum.photos/200/250";
       array.push({
+        id: i,
         productName: `Product-${i}`,
         price: Math.floor(Math.random() * 100),
         quantity: Math.floor(Math.random() * 10),
         imageUrl,
+        bought: false,
       });
     }
     return array;
   };
 
-  useEffect( () => {
+  const buyItem = (id) => {
+    const newProductList = productList.map((item) =>
+      item.id == id ? { ...item, bought: true } : item
+    );
+    setProduct(newProductList);
+  };
+
+  useEffect(() => {
     const temp = getProducts();
     setProduct(temp);
   }, []);
 
   return (
     <div className={styles.cardHolder}>
-      {productList.map(({ productName, price,quantity, imageUrl }) => {
-        return (
-            <div className={styles["mantra-vertical-card"]}>
+      {productList.map(
+        ({ id, productName, price, quantity, imageUrl, bought }) => {
+          return (
+            <div keys={id} className={styles["mantra-vertical-card"]}>
               <div className={styles["mantra-card-holder-image-v"]}>
                 <img className={styles["mantra-vert-image"]} src={imageUrl} />
               </div>
@@ -38,23 +48,30 @@ export const ProductCard = () => {
                 </div>
               </div>
               <div className={styles["mantra-card-btn"]}>
-                {quantity >0 ?
-                <button
-                  className={styles["mantra-button"]}
-                >
-                  <i className="fa fa-shopping-cart"></i>
-                  Add to cart
-                </button>: <button
-                  className={styles["mantra-button"]}
-                  disabled={true}
-                >
-                 Out of Stock
-                </button>
-                }
+                {quantity > 0 ? (
+                  bought ? (
+                    <button disabled={true} className={styles["mantra-button"]}>
+                      Already Bought!!
+                    </button>
+                  ) : (
+                    <button
+                      className={styles["mantra-button"]}
+                      onClick={() => buyItem(id)}
+                    >
+                      <i className="fa fa-shopping-cart"></i>
+                      Add to cart
+                    </button>
+                  )
+                ) : (
+                  <button className={styles["mantra-button"]} disabled={true}>
+                    Out of Stock
+                  </button>
+                )}
               </div>
-          </div>
-        );
-      })}
+            </div>
+          );
+        }
+      )}
     </div>
   );
 };
